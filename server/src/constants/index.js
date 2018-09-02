f3d00d615426a39cfc7c879344d1ced39fc4b8b1
Scott Hardy <hardy613@gmail.com>
2018-09-02 23:48:12 +0000

    fixing exports

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -1,3 +1,7 @@
-export const MAX_SHORT_URL_LENGTH = 5
-export const APP_SECRET = process.env.APP_SECRET
+const MAX_SHORT_URL_LENGTH = 5
+const APP_SECRET = process.env.APP_SECRET
 
+module.export = {
+	MAX_SHORT_URL_LENGTH,
+	APP_SECRET,
+}
